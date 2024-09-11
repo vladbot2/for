72d406e1b27db902dca5e7f715f2ca01fc9551a7
dev <dev@A21PC12>
2024-09-11 17:42:27 +0300

    Add project files.

---
+++ for.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //2
+        //int n = 5;
+        //for (int i = 1; i <= n; i += 2) {
+        //    for (int j = 0; j < (n - i) / 2; ++j) {
+        //        cout << " ";  
+        //    }
+        //    for (int j = 0; j < i; ++j) {
+        //        cout << "*";  
+        //    }
+        //    cout << endl;
+        //}
+
+        //for (int i = n - 2; i >= 1; i -= 2) {
+        //    for (int j = 0; j < (n - i) / 2; ++j) {
+        //        cout << " ";  
+        //    }
+        //    for (int j = 0; j < i; ++j) {
+        //        cout << "*";  
+        //    }
+        //    cout << endl;
+        /*}*/
+
+//С.м 1
+		/*int lower_bound = 10;
+		int upper_bound = 20;
+		int number;
+
+		cout << "Enter a number between " << lower_bound << " and " << upper_bound << ": ";
+		cin >> number;
+
+		while (number < lower_bound || number > upper_bound) {
+			cout << "Number is out of range. Please try again: ";
+			cin >> number;
+		}
+
+		cout << "Number " << number << " is within the range!" << endl;*/
+}
+
